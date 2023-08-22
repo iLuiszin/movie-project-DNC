06d6f0900472d6e45d1a6a7aa2ebaadf7afa7597
Oluis <Oluis10@gmail.com>
2023-08-21 21:43:35 -0300

    feat: finishing search logic

--- src/main.jsx
+++ src/main.jsx
@@ -1,34 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {
-  createBrowserRouter,
-  RouterProvider,
+  BrowserRouter
 } from "react-router-dom";
 import './index.scss'
 import App from './App';
-import Home from './pages/Home/Home';
-import MovieDetail from './pages/MovieDetail/MovieDetail';
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "",
-        element: <Home />
-      },
-      {
-        path: "/movie/:id",
-        element: <MovieDetail />
-      }
-    ]
-  },
-
-]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter >
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,11 +2,16 @@ import { useEffect, useState } from 'react'
 import { MovieService } from '../../api/MovieService'
 import MovieCard from '../../components/MovieCard/MovieCard';
 
-const Home = () => {
+const Home = ({ searchValue }) => {
     const [movies, setMovies] = useState([])
 
     async function getMovies() {
         const { data: { results } } = await MovieService.getMovies();
+        setMovies(results)
+    }
+
+    async function getMoviesSearch(movieName) {
+        const { data: { results } } = await MovieService.searchMovies(movieName);
         setMovies(results)
     }
 
@@ -15,10 +20,14 @@ const Home = () => {
     }, [])
 
     useEffect(() => {
-        console.log(movies)
-    },)
+        if (searchValue) {
+            getMoviesSearch(searchValue)
+        }
 
-
+        if (searchValue === '') {
+            getMovies()
+        }
+    }, [searchValue])
 
     return (
         <section className="Home">
